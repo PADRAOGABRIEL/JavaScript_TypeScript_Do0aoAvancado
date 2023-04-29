4e6862bde1cebc581744e8acc6a9608d7e21ea44
Padrao <padraogabriel20@gmail.com>
2023-04-29 11:24:37 -0300

    password generator

---
+++ Ex22/src/main.js
@@ -0,0 +1,10 @@
+/*import { nome,sobrenome, idade,soma } from "./modulo1";
+
+console.log(nome, sobrenome, idade)
+console.log(soma(40,3))*/
+
+import  forms from './modules/forms'
+import './assets/css/style.css'
+
+forms()
+
